314dd080f494ec2a76d2e83dc59f67e2a356f143
ondjj <jcjk0302@gmail.com>
2023-01-17 16:42:30 +0900

    엔티티 클래스 개발 - 2

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -6,11 +6,18 @@ import lombok.Setter;
 import javax.persistence.Embeddable;
 
 @Embeddable
-@Getter @Setter
+@Getter
 public class Address {
 
     private String city;
     private String street;
     private String zipcode;
 
+    protected Address() {
+    }
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }
--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -32,4 +33,20 @@ public class Order {
 
     @Enumerated(EnumType.STRING)
     private OrderStatus status; // 주문 상 [ORDER, CANCEL]
+
+    //==연관관계 메서드==//
+//    public void setMember(Member member) {
+//        this.member = member;
+//        member.getOrders().add(this);
+//    }
+//    public void addOrderItem(OrderItem orderItem) {
+//
+//
+//        orderItem.setOrder(this);
+//    }
+//    public void setDelivery(Delivery delivery) {
+//        this.delivery = delivery;
+//        delivery.setOrder(this);
+//    }
+
 }
